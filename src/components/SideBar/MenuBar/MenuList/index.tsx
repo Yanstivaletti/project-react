8570f70df812e1814a56c246d1ac66e0ec872c0f
yanstivaletti <yanstivalettiesouza15@gmail.com>
2022-03-04 09:05:23 -0300

    resolvendo o botão toggle

--- src/components/SideBar/MenuBar/MenuList/index.tsx
+++ src/components/SideBar/MenuBar/MenuList/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import { MenuItem } from './menuItem';
+import React, { useState } from 'react';
 import { FaHome, FaUsers } from 'react-icons/fa';
+import { useMyUsers } from '../../../../Context/usersContext';
+import { ToggleSwitch } from '../../../ToggleSwitch';
+import { MenuItem } from './menuItem';
 import { List } from './style';
 
 const MenuList: React.FC = () => {
+
+  const [check, setCheck] = useState(false);
+  const { setDarkTheme } = useMyUsers();
+
+  const handleClick = () => {
+    setCheck((check) => !check);
+    console.log('cheguei', check);
+
+    setDarkTheme((check) => !check);
+  };
   return (
     <List>
       <MenuItem Icon={FaHome} title="Início" path="/" />
       <MenuItem Icon={FaUsers} title="Usuários" path="/users" />
+      <ToggleSwitch onClick={handleClick} />
+
     </List>
   );
 };
--- src/components/SideBar/MenuBar/index.tsx
+++ src/components/SideBar/MenuBar/index.tsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
-import { useMyUsers } from '../../../Context/usersContext';
-import { ToggleSwitch } from '../../ToggleSwitch';
+import React from 'react';
 import MenuList from './MenuList';
 import { WrapperMenuBar } from './style';
 
 const MenuBar: React.FC = () => {
-  const [check, setCheck] = useState(false);
-  const { setDarkTheme } = useMyUsers();
 
-  const handleClick = () => {
-    setCheck((check) => !check);
-    console.log('cheguei', check);
-
-    setDarkTheme((check) => !check);
-  };
 
   return (
     <WrapperMenuBar>
       <MenuList />
-      <ToggleSwitch onClick={handleClick} />
     </WrapperMenuBar>
   );
 };
